a15c2fb76a415affd0df3f59b20daa4513b4eea9
fti-aghodake <109514349+fti-aghodake@users.noreply.github.com>
2022-12-13 16:48:22 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,10 +11,10 @@ git --version
 echo 4. Who is running your script?
 whoami
  
-echo 5.
-uname -r
+echo 5. Displays the operating system name as well as the system node name, operating system release, operating system version, hardware name, and processor type. 
+uname -a
 
-echo 6. 
+echo 6. To display system information(Displays the operating system name).
 uname
 
 
