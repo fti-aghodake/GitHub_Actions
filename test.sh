d84d4359a2ec0ef4c49f9dff0e4eb43f04094b76
fti-aghodake <109514349+fti-aghodake@users.noreply.github.com>
2022-12-13 11:19:00 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 1. what is in this directory?
+ls -a
+echo
+echo 2. Is java Installed?
+java --version
+echo 
+echo 3. Is Git Installed?
+git --version
+echo 4. Who is running your script?
+whoami
