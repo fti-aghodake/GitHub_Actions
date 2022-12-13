fcf4971e1fee2ad0468587dfa31e1324bcc1b41c
fti-aghodake <109514349+fti-aghodake@users.noreply.github.com>
2022-12-13 17:05:35 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,11 +3,14 @@
 echo 1. what is in this directory?
 ls -a
 echo
+
 echo 2. Is java Installed?
 java --version
-echo 
+echo
+
 echo 3. Is Git Installed?
 git --version
+
 echo 4. Who is running your script?
 whoami
  
