96e43e652c3b763c32188f66e535599c8ffee775
fti-aghodake <109514349+fti-aghodake@users.noreply.github.com>
2022-12-13 16:36:56 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,17 +11,8 @@ git --version
 echo 4. Who is running your script?
 whoami
  
-echo 5. what is this in this script?
-echo "Hello world" # this is also a comment
-
-echo Our shell name is $BASH
-echo Our shell version is 5.1.16(1)-release
-echo Our home directory is /home/amol
-echo Our current working directory is /home/amol/Downloads/Shell_script
-
-#user defined variable in shell script
-name=Mark
-echo The name is Mark
+echo 5. what is time?
+start_time=$(time +%F' 00:00:00'  -d "-$i days")
 
 
 
