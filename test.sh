e997d4c0ba2209b9fd5dfef56bf979a25fad3802
fti-aghodake <109514349+fti-aghodake@users.noreply.github.com>
2022-12-13 16:30:33 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,3 +10,17 @@ echo 3. Is Git Installed?
 git --version
 echo 4. Who is running your script?
 whoami
+ 
+echo 5. what is this in this script?
+echo "Hello world" # this is also a comment
+
+echo Our shell name is $BASH
+echo Our shell version is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our current working directory is $PWD
+
+#user defined variable in shell script
+name=Mark
+echo The name is $name
+
+
