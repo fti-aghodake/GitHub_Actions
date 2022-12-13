efb09c7263114fcbd0b9d0da9cfaa25bd94783b5
fti-aghodake <109514349+fti-aghodake@users.noreply.github.com>
2022-12-13 17:12:04 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,10 +11,13 @@ echo
 echo 3. Is Git Installed?
 git --version
 
-echo 4. Who is running your script?
+echo 4. Is Python3 installed?
+python3 --version
+
+echo 5. Who is running your script?
 whoami
  
-echo 5. To display system info.
+echo 6. To display system info.
 uname -a
 
 
